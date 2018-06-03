0669b4e615e27846688e91fff24504c278d9a3c6
Rictusempra <16912240+Rictusempra@users.noreply.github.com>
2018-06-03 22:48:20 +0800

    listen to bookmark

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
+function handleChanged(id, changeInfo) {
+    console.log("Item: " + id + " changed");
+    console.log("Title: " + changeInfo.title);
+    console.log("Url: " + changeInfo.url);
+}
+
+// listen for bookmarks being updated
+browser.bookmarks.onChanged.addListener(listener)
+
 // listen for bookmarks being created
 browser.bookmarks.onCreated.addListener(updateActiveTab);
 
