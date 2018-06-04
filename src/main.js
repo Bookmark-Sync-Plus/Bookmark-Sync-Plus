d8b9df5259b497956cd77a23e58707a8e8f85319
Rictusempra <16912240+Rictusempra@users.noreply.github.com>
2018-06-04 11:19:59 +0800

    open option page

--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
 //set badge title text
-function synctime (second, minute, hour, day, month, year) {
-}
-var t1 = new synctime(second, minute, hour, day, month, year);
 browser.browserAction.setTitle({
     title: 'Last sync time: ' + 'Never'
 });
+/*function synctime (second, minute, hour, day, month, year) {
+};
+var t1 = new synctime(second, minute, hour, day, month, year);*/
+
+//option page
+function handleClick() {
+    browser.runtime.openOptionsPage();
+   }
+browser.browserAction.onClicked.addListener(handleClick);
+//OAuth
+
+// listen for bookmarks being updated
+browser.bookmarks.onChanged.addListener(listener);
+
+// listen for bookmarks being created
+browser.bookmarks.onCreated.addListener(listener);
+
+// listen for bookmarks being removed
+browser.bookmarks.onRemoved.addListener(listener);
+
 
 //get bookmark properties
 function handleChanged(id, changeInfo) {
@@ -12,12 +29,3 @@ function handleChanged(id, changeInfo) {
     console.log("Title: " + changeInfo.title);
     console.log("Url: " + changeInfo.url);
 }
-
-// listen for bookmarks being updated
-browser.bookmarks.onChanged.addListener(listener)
-
-// listen for bookmarks being created
-browser.bookmarks.onCreated.addListener(listener);
-
-// listen for bookmarks being removed
-browser.bookmarks.onRemoved.addListener(listener);
