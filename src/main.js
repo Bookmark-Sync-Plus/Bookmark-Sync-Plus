0f57222f5893939abd28b8d646ccca97075f7738
Rictusempra <16912240+Rictusempra@users.noreply.github.com>
2018-06-04 13:14:27 +0800

    better files structure

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+//compatibility hack
+var browser = chrome || browser;
 //set badge title text
 browser.browserAction.setTitle({
     title: 'Last sync time: ' + 'Never'
@@ -7,9 +9,23 @@ browser.browserAction.setTitle({
 var t1 = new synctime(second, minute, hour, day, month, year);*/
 
 //option page
+function openURL(url)
+{
+    chrome.tabs.create({
+        url: url
+    })
+}
+
+chrome.runtime.onInstalled.addListener(function () {
+    browser.runtime.openOptionsPage();
+});
+
 function handleClick() {
     browser.runtime.openOptionsPage();
-   }
+}
+
+function listener () {}
+
 browser.browserAction.onClicked.addListener(handleClick);
 //OAuth
 
@@ -24,8 +40,9 @@ browser.bookmarks.onRemoved.addListener(listener);
 
 
 //get bookmark properties
-function handleChanged(id, changeInfo) {
+/*function handleChanged(id, changeInfo) {
     console.log("Item: " + id + " changed");
     console.log("Title: " + changeInfo.title);
     console.log("Url: " + changeInfo.url);
 }
+*/
