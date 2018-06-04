c61e972e4ffd9234c6d4dae31346a63e41183f3d
Rictusempra <16912240+Rictusempra@users.noreply.github.com>
2018-06-04 14:29:49 +0800

    listen to create/remove

--- src/main.js
+++ src/main.js
@@ -7,10 +7,9 @@ browser.browserAction.setTitle({
 /*function synctime (second, minute, hour, day, month, year) {
 };
 var t1 = new synctime(second, minute, hour, day, month, year);*/
-
+function listener() {}
 //option page
-function openURL(url)
-{
+function openURL(url) {
     chrome.tabs.create({
         url: url
     })
@@ -24,8 +23,6 @@ function handleClick() {
     browser.runtime.openOptionsPage();
 }
 
-function listener () {}
-
 browser.browserAction.onClicked.addListener(handleClick);
 //OAuth
 
@@ -35,9 +32,22 @@ browser.bookmarks.onChanged.addListener(listener);
 // listen for bookmarks being created
 browser.bookmarks.onCreated.addListener(listener);
 
+function handleCreated(id, bookmarkInfo) {
+    console.log(`New bookmark ID: ${id}`);
+    console.log(`New bookmark URL: ${bookmarkInfo.url}`);
+    console.log(`New bookmark URL: ${bookmarkInfo.title}`);
+}
+browser.bookmarks.onCreated.addListener(handleCreated);
+
 // listen for bookmarks being removed
 browser.bookmarks.onRemoved.addListener(listener);
 
+function handleRemoved(id, bookmarkInfo) {
+    console.log(`New bookmark ID: ${id}`);
+    console.log(`New bookmark URL: ${bookmarkInfo.url}`);
+    console.log(`New bookmark URL: ${bookmarkInfo.title}`);
+}
+browser.bookmarks.onRemoved.addListener(handleRemoved);
 
 //get bookmark properties
 /*function handleChanged(id, changeInfo) {
